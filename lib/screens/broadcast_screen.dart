3004b2b282d876ec0fff4e5e71aa1ad32018e661
abhijeetSaroha <abhijeetsaroha08@gmail.com>
2022-09-04 08:06:10 +0530

    update the broadcast screen

--- lib/screens/broadcast_screen.dart
+++ lib/screens/broadcast_screen.dart
@@ -1,8 +1,12 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:agora_rtc_engine/rtc_engine.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_live/config/appId.dart';
 import 'package:go_live/providers/user_provider.dart';
+import 'package:go_live/resources/firestore_methods.dart';
+import 'package:go_live/screens/home_screen.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 import 'package:agora_rtc_engine/rtc_local_view.dart' as RtcLocalView;
@@ -22,6 +26,7 @@ class BroadcastScreen extends StatefulWidget {
 class _BroadcastScreenState extends State<BroadcastScreen> {
   List<int> remoteUid = [];
   late final RtcEngine _engine;
+
   @override
   void initState() {
     _initEngine();
@@ -64,7 +69,7 @@ class _BroadcastScreenState extends State<BroadcastScreen> {
     }
     await _engine.joinChannelWithUserAccount(
       tempToken,
-      'test123',
+      'testing123',
       Provider.of<UserProvider>(context, listen: false).user.uid,
     );
   }
@@ -83,15 +88,32 @@ class _BroadcastScreenState extends State<BroadcastScreen> {
     }
   }
 
+  _leaveChannel() async {
+    await _engine.leaveChannel();
+    if ('${Provider.of<UserProvider>(context, listen: false).user.uid}${Provider.of<UserProvider>(context, listen: false).user.username}' ==
+        widget.channelId) {
+      await FirestoreMethods().endLiveStream(widget.channelId);
+    } else {
+      await FirestoreMethods().updateViewCount(widget.channelId, false);
+    }
+    Navigator.pushReplacementNamed(context, HomeScreen.routeName);
+  }
+
   Widget build(BuildContext context) {
     final user = Provider.of<UserProvider>(context).user;
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8),
-        child: Column(
-          children: [
-            _renderVideo(user),
-          ],
+    return WillPopScope(
+      onWillPop: () async {
+        await _leaveChannel();
+        return Future.value(true);
+      },
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            children: [
+              _renderVideo(user),
+            ],
+          ),
         ),
       ),
     );
